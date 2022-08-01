554682935d1bb5ac3358765be11cd1ec43c4c752
alfaonyt <62027587+alfaonyt@users.noreply.github.com>
2022-08-01 19:35:15 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/asus/ASUS_I005_1
+LOCAL_PATH := device/asus/I005_1
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \

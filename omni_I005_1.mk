ab90ce5dff20804caa46c853069636490a5dbbdd
alfaonyt <62027587+alfaonyt@users.noreply.github.com>
2022-08-01 19:36:01 +0200

    Update and rename omni_ASUS_I005_1.mk to omni_I005_1.mk

--- omni_I005_1.mk
+++ omni_I005_1.mk
@@ -10,16 +10,16 @@ $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# Inherit from ASUS_I005_1 device
-$(call inherit-product, device/asus/ASUS_I005_1/device.mk)
+# Inherit from I005_1 device
+$(call inherit-product, device/asus/I005_1/device.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := ASUS_I005_1
-PRODUCT_NAME := omni_ASUS_I005_1
+PRODUCT_DEVICE := I005_1
+PRODUCT_NAME := omni_I005_1
 PRODUCT_BRAND := asus
-PRODUCT_MODEL := ASUS_I005D
+PRODUCT_MODEL := I005D
 PRODUCT_MANUFACTURER := asus

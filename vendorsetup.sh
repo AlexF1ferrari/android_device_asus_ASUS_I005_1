a5ad7ce05c26ce9084e611181b944cdfc54a4d8c
alfaonyt <62027587+alfaonyt@users.noreply.github.com>
2022-08-01 19:36:31 +0200

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_ASUS_I005_1-user
-add_lunch_combo omni_ASUS_I005_1-userdebug
-add_lunch_combo omni_ASUS_I005_1-eng
+add_lunch_combo omni_I005_1-user
+add_lunch_combo omni_I005_1-userdebug
+add_lunch_combo omni_I005_1-eng
